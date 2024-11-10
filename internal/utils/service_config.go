9850d9f1ec91733198f6763f20b090f255b34ff4
amir <a.h.amani.t@gmail.com>
2024-11-10 14:57:16 +0330

    add README and some config

--- internal/utils/service_config.go
+++ internal/utils/service_config.go
@@ -57,9 +57,39 @@ func SetupViper(path string) (*Config, error) {
 		return nil, errors.New(msg)
 	}
 	var c Config
+
+	SetViperEnvMappings()
+
 	err = viper.Unmarshal(&c)
 	if err != nil {
 		return nil, err
 	}
 	return &c, nil
+}
+
+func SetViperEnvMappings() {
+	// Set environment variable mappings
+	viper.SetEnvPrefix("APP") // Optional: adds APP_ prefix to all env variables
+
+	// Map environment variables to config fields
+	viper.BindEnv("server.port", "APP_SERVER_PORT")
+	viper.BindEnv("server.host", "APP_SERVER_HOST")
+	viper.BindEnv("server.mode", "APP_SERVER_MODE")
+
+	viper.BindEnv("db.host", "APP_DB_HOST")
+	viper.BindEnv("db.port", "APP_DB_PORT")
+	viper.BindEnv("db.user", "APP_DB_USER")
+	viper.BindEnv("db.password", "APP_DB_PASSWORD")
+	viper.BindEnv("db.dbname", "APP_DB_NAME")
+	viper.BindEnv("db.sslmode", "APP_DB_SSLMODE")
+
+	viper.BindEnv("redis.host", "APP_REDIS_HOST")
+	viper.BindEnv("redis.port", "APP_REDIS_PORT")
+	viper.BindEnv("redis.password", "APP_REDIS_PASSWORD")
+	viper.BindEnv("redis.db", "APP_REDIS_DB")
+
+	viper.BindEnv("jwt.secret", "APP_JWT_SECRET")
+	viper.BindEnv("jwt.expire_hour", "APP_JWT_EXPIRE_HOUR")
+
+	viper.BindEnv("log_level", "APP_LOG_LEVEL")
 }
