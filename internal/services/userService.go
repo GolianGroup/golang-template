d12dd379bf83c5b409bff0eac880f09d92935189
amir <a.h.amani.t@gmail.com>
2024-11-10 13:56:24 +0330

    fix all files to snake_case

--- internal/services/userService.go
+++ internal/services/userService.go
@@ -17,6 +17,6 @@ func NewUserService(repo repositories.UserRepository) UserService {
 	return &userService{repo: repo}
 }
 
-func (s userService) Login(user dto.User) {
+func (s *userService) Login(user dto.User) {
 	s.repo.Get(user)
 }
