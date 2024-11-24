ea2078acb3f509c5b1343b85c1c99733122b5f1f
20Parastesh01 <parastesh.al2001@gmail.com>
2024-11-24 16:13:07 +0330

    Relocate migration.go and migrations dir and Rename Postgres CLI tools

--- app/database.go
+++ app/database.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"golang_template/internal/database/arango"
 	"golang_template/internal/database/postgres"
 	"log"
 )
@@ -9,6 +10,14 @@ func (a *application) InitDatabase() postgres.Database {
 	db, err := postgres.NewDatabase(a.ctx, &a.config.DB)
 	if err != nil {
 		log.Fatalf("failed to setup database: %s", err)
+	}
+	return db
+}
+
+func (a *application) InitArangoDB() arango.ArangoDB {
+	db, err := arango.NewArangoDB(a.ctx, &a.config.ArangoDB)
+	if err != nil {
+		log.Fatalf("failed to setup arango database: %s", err)
 	}
 	return db
 }
