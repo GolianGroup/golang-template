d2a72b839f7ddf3ea5f9fb0c50d44fd278171684
alvandashhadi <alvandashhadi5@gmail.com>
2024-11-25 11:59:48 +0330

    feat(back):add ClickHouse database initialization to application

--- app/database.go
+++ app/database.go
@@ -1,14 +1,23 @@
 package app
 
 import (
-	"golang_template/internal/database"
+	"golang_template/internal/database/clickhouse"
+	"golang_template/internal/database/postgres"
 	"log"
 )
 
-func (a *application) InitDatabase() database.Database {
-	db, err := database.NewDatabase(a.ctx, &a.config.DB)
+func (a *application) InitPostgresDatabase() postgres.PostgresDatabase {
+	db, err := postgres.NewPostgresDatabase(a.ctx, &a.config.Postgres)
 	if err != nil {
-		log.Fatalf("failed to setup database: %s", err)
+		log.Fatalf("failed to setup postgres database: %s", err)
+	}
+	return db
+}
+
+func (a *application) InitClickhouseDatabase() clickhouse.ClickhouseDatabase {
+	db, err := clickhouse.NewClickhouseDatabase(a.ctx, &a.config.Clickhouse)
+	if err != nil {
+		log.Fatalf("failed to setup clickhouse database: %s", err)
 	}
 	return db
 }
